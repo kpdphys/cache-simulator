86162506d42bad255a063444a522398e788aedf0
Kravchenya Pavel <kpd@example.com>
2025-05-22 20:27:16 +0300

    fix(core): release featues
    Big release features.

---
+++ cache_simulator/mycode.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""_summary_."""
+
+
+def func() -> int:
+    """_summary_.
+
+    Returns:
+        int: _description_
+
+    """
+    return 5
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,9 @@
-import pytest
+"""Module docstring."""
+
+from cache_simulator.mycode import func
 
 
-def test_code_is_tested():
-    assert True
+def test_code_is_tested() -> None:
+    """Docstring for function."""
+    assert func() == 5
     # test
